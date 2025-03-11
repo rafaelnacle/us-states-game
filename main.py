56b516c9ed8a67fc10bf478adcd62a3cab48405d
= <=>
2025-03-11 15:58:33 -0300

    Create csv for all missing countries that weren't guessed

--- main.py
+++ main.py
@@ -14,7 +14,17 @@ all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name? (Type 'Exit' to leave)").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
 
     if answer_state in all_states:
         guessed_states.append(answer_state)
@@ -24,5 +34,3 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(state_data.x.item(), state_data.y.item())
         t.write(answer_state) # or using the panda way: state_data.state.item()
-
-screen.mainloop()
